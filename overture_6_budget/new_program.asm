ee510278141bc4130adfae4259d6071643ffad1e
Niels Boehm <blubberdiblub@gmail.com>
2026-07-20 13:09:33 +0200

    v2.1.264

---
+++ overture_6_budget/new_program.asm
@@ -0,0 +1,256 @@
+mov r0, in
+mov r1, in
+mov r2, in
+mov r4, in
+mov r5, in
+or
+mov r1, r3
+mov r2, r3
+nand
+mov r1, r3
+or
+mov r1, r3
+mov r2, r3
+nor
+mov r1, r3
+mov r2, r3
+and
+mov r1, r3
+nor
+mov r2, r3
+mov out, r0
+mov r0, in
+mov r1, r3
+sub
+mov r1, r3
+mov r2, r3
+sub
+mov r1, r3
+nop
+or
+mov r0, r3
+mov r1, r3
+and
+mov r1, r3
+or
+mov r2, r3
+and
+mov r2, in
+mov out, r0
+mov r0, r3
+sub
+mov r2, r3
+and
+mov r1, in
+mov r2, r3
+add
+mov out, r0
+imm 50
+mov r2, r0
+mov r0, in
+jz
+mov r0, r3
+mov r2, r3
+nand
+mov r1, r3
+mov r2, r3
+nor
+mov r2, r3
+nand
+mov r2, r3
+or
+mov r1, r3
+nor
+mov r1, r3
+mov r2, r3
+add
+mov r2, r3
+mov r1, r3
+add
+mov r2, r3
+or
+mov r2, r3
+mov r1, r3
+or
+mov r1, r3
+sub
+mov r2, r3
+mov r1, r3
+and
+mov r1, r3
+mov out, r0
+mov r0, in
+mov r2, r3
+add
+jnz
+mov r0, r3
+mov r2, r3
+nand
+mov r2, r3
+and
+mov r2, r3
+and
+mov r2, r3
+sub
+mov r1, r3
+mov r2, r3
+add
+mov r2, r3
+mov r1, r3
+sub
+mov r1, r3
+mov r2, r3
+nand
+mov r1, r3
+mov r2, r3
+sub
+mov r2, in
+mov out, r4
+mov r4, r3
+and
+mov r2, r3
+mov r1, r3
+mov out, r0
+mov r0, in
+add
+jl
+mov r1, r3
+sub
+mov r2, r3
+mov r0, r3
+mov r1, r3
+and
+mov r1, r3
+mov r2, in
+nor
+mov r2, r3
+add
+mov r2, r3
+and
+mov r1, r3
+mov r2, r3
+nand
+mov r2, r3
+add
+mov r1, r3
+and
+mov r1, r3
+nor
+mov r1, r3
+mov r2, r3
+sub
+mov r1, r3
+mov r2, r3
+or
+mov r2, in
+mov out, r0
+mov out, r1
+imm 11
+mov r1, r0
+mov r0, r3
+mov r1, in
+and
+mov r1, r3
+jge
+and
+mov r0, r3
+mov r2, in
+and
+mov r1, r3
+mov r2, r3
+nor
+mov r1, r3
+mov r2, r3
+and
+mov r2, r3
+nand
+mov r2, r3
+and
+mov r2, r3
+nor
+mov r1, r3
+nand
+mov r1, r3
+mov r2, r3
+add
+mov r1, r3
+mov out, r0
+mov r0, in
+nand
+jle
+mov r0, r3
+mov r1, in
+nor
+mov r2, in
+mov r1, r3
+or
+mov r2, r3
+mov r1, r3
+nor
+mov r1, r3
+mov r2, in
+add
+mov r2, r3
+mov r1, r3
+or
+mov r2, r3
+mov r1, r3
+add
+mov r1, r3
+and
+mov r1, r3
+and
+mov r2, r3
+nand
+mov r1, in
+mov out, r4
+mov r4, in
+mov out, r0
+mov r0, r3
+nand
+mov r1, r3
+mov r2, r3
+nand
+mov out, r0
+mov r0, in
+mov r1, r3
+or
+mov r1, r3
+mov r2, r3
+nor
+mov r2, r3
+jg
+sub
+mov r1, r3
+mov r2, r3
+mov r0, r3
+or
+mov r1, r3
+mov r2, r3
+and
+mov r2, r3
+mov r1, r3
+add
+mov r1, r3
+and
+mov r2, r3
+nor
+mov r1, r3
+sub
+mov r1, r3
+mov r2, r3
+or
+mov r2, r3
+nand
+mov r2, r3
+sub
+mov r1, r3
+mov out, r0
+mov r0, in
+mov r2, r3
+nor
+mov r1, r3
+and
+mov r2, r3
+nor
+jmp

79b0eb1fb6b82b86dbc16e2ecced15e4d8a2669f
Niels Boehm <blubberdiblub@gmail.com>
2026-08-09 16:38:51 +0200

    v2.1.303

--- assembly_programming/new_program.asm
+++ assembly_programming/new_program.asm
@@ -1,6 +1,6 @@
 ; (31337_41299869859669, `This is a comment`)
 
-mov r2, in ; (31337_73562005049347, `Copies input to reg 2`)
+mov r2, in ; (31337_73562005049347, `Moves input to reg 2`)
 imm 15 ; (31337_36228934755685, `Moves 15 to reg 0`)
 
-; (31337_80524429403045, `Click "ASM" in the top left to see all instructions`)
+; (31337_80524429403045, `Click the spec.isa tab in the top left to see all instructions`)
